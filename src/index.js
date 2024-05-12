320e9c6eb998110cd3c2432426f521efdd5b9065
Satoko Gruson <satokogruson@gmail.com>
2024-05-12 11:14:27 +0200

    fixed minor codes for css

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function generatePoem(event) {
     event.preventDefault();
 let instructionsInput= document.querySelector("#user-instructions")
     let apiKey = "10b545o25teaa28dd38fd076fc778f2c";
-    let context = "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br/>. Please follow the users instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI in bold inside a <strong> element at the end of the poem and NOT at the beginning";
+    let context = "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br/>. Please follow the users instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' in bold inside a <strong> element at the end of the poem and NOT at the beginning";
     let prompt = `User instructions: Generate a French poem about the French poem about ${instructionsInput.value}`;
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
