d6f01aa134a4ba9718fd719607d93362e43789a9
Satoko Gruson <satokogruson@gmail.com>
2024-05-13 13:35:08 +0200

    added blinking text

--- src/index.js
+++ src/index.js
@@ -10,11 +10,16 @@ function displayPoem(response) {
 
 function generatePoem(event) {
     event.preventDefault();
-let instructionsInput= document.querySelector("#user-instructions")
+    let instructionsInput = document.querySelector("#user-instructions")
     let apiKey = "10b545o25teaa28dd38fd076fc778f2c";
     let context = "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br/>. Please follow the users instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' in bold inside a <strong> element at the end of the poem and NOT at the beginning";
     let prompt = `User instructions: Generate a French poem about the French poem about ${instructionsInput.value}`;
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+
+    let poemElement = document.querySelector('#poem');
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div class="generating">⌛️ Generating the French Poem about ${instructionsInput.value}</div>`;
 
     console.log("Generating poem");
     console.log(`Prompt:${prompt}`);
